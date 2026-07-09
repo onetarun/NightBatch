151cb22f8fda121890f30f13473b26e3951e4627
Tarun Saini <onetarun@outlook.com>
2026-07-09 21:30:01 +0530

    Add project files.

---
+++ NightBatch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NightBatch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
