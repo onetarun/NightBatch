83fc75e5fafce11f3850148554d30555dcc3cf61
Tarun Saini <onetarun@outlook.com>
2026-07-09 22:19:08 +0530

    Add DB

--- NightBatch/Controllers/HomeController.cs
+++ NightBatch/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using NightBatch.Data;
 using NightBatch.Models;
 
 namespace NightBatch.Controllers
@@ -7,15 +8,18 @@ namespace NightBatch.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var students = _context.Students.ToList();
+            return View(students);
         }
 
         public IActionResult Privacy()
